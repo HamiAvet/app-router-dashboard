4e6499b0c5cf1dd41dea2be3ef8ff4bca9ce75f0
HamiAvet <hamov2003@gmail.com>
2025-04-09 16:42:02 +0200

    hhh

--- nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import {
   UserGroupIcon,
   HomeIcon,
@@ -15,8 +17,12 @@ const links = [
   },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
+import clsx from 'clsx';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function NavLinks() {
+  const pathname = usePathname()
   return (
     <>
       {links.map((link) => {
@@ -25,7 +31,11 @@ export default function NavLinks() {
           <a
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            className={clsx("flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+              {
+                'bg-sky-100 text-blue-600': pathname === link.href
+              },
+            )}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
